37207c0ad17ed0ad386cc4c4215124389e5e742e
tashbash59 <tashbaev300@gmail.com>
2025-04-17 11:57:34 +0300

    remove ckedito

--- SDO/SDO/urls.py
+++ SDO/SDO/urls.py
@@ -7,5 +7,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
